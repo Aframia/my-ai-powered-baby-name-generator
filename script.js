cc81d074e5fca3c5f7ebaf617207177ee11a80d8
Aframia <168097776+Aframia@users.noreply.github.com>
2025-07-27 19:17:56 +0000

    imported typewriter plugin and added style

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let apiKey ="fb820fo1b1cfa408bca4ft48b00334dc";
-let apiUrl ="https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
+let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 axios.get(apiUrl).then(sendMessage);
